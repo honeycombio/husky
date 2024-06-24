2f4b008c950f0455c6715ae8f30112977aa59bd4
Robb Kidd <robbkidd@honeycomb.io>
2024-06-24 15:10:44 -0700

    refactor switch to a map
    We like maps. Maps are fast.

--- otlp/semconv.go
+++ otlp/semconv.go
@@ -1,23 +1,20 @@
 package otlp
 
+var descriptions = map[string]string{
+	"http.request.method":          "HTTP request method.",
+	"http.request.header":          "HTTP request headers, `<key>` being the normalized HTTP Header name (lowercase), the value being the header values.",
+	"http.request.method_original": "Original HTTP method sent by the client in the request line.",
+	"http.request.resend_count":    "The ordinal number of request resending attempt (for any reason, including redirects).",
+	"http.response.header":         "HTTP response headers, `<key>` being the normalized HTTP Header name (lowercase), the value being the header values.",
+	"http.response.status_code":    "[HTTP response status code](https://tools.ietf.org/html/rfc7231#section-6).",
+	"http.route":                   "The matched route, that is, the path template in the format used by the respective server framework.",
+}
+
 // GetDescriptionForOtelAttributeName returns an informative description of a given OpenTelemetry attribute name.
 func GetDescriptionForOtelAttributeName(name string) string {
-	switch name {
-	case "http.request.method":
-		return "HTTP request method."
-	case "http.request.header":
-		return "HTTP request headers, `<key>` being the normalized HTTP Header name (lowercase), the value being the header values."
-	case "http.request.method_original":
-		return "Original HTTP method sent by the client in the request line."
-	case "http.request.resend_count":
-		return "The ordinal number of request resending attempt (for any reason, including redirects)."
-	case "http.response.header":
-		return "HTTP response headers, `<key>` being the normalized HTTP Header name (lowercase), the value being the header values."
-	case "http.response.status_code":
-		return "[HTTP response status code](https://tools.ietf.org/html/rfc7231#section-6)."
-	case "http.route":
-		return "The matched route, that is, the path template in the format used by the respective server framework."
-	default:
-		return ""
+	if description, ok := descriptions[name]; ok {
+		return description
 	}
+
+	return ""
 }
