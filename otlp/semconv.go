7de599a4fd402475d2506fea2ad566db9093d44e
Robb Kidd <robbkidd@honeycomb.io>
2024-06-24 14:33:46 -0700

    yea, verily, the tests shall pass

--- otlp/semconv.go
+++ otlp/semconv.go
@@ -7,6 +7,16 @@ func GetDescriptionForOtelAttributeName(name string) string {
 		return "HTTP request method."
 	case "http.request.header":
 		return "HTTP request headers, `<key>` being the normalized HTTP Header name (lowercase), the value being the header values."
+	case "http.request.method_original":
+		return "Original HTTP method sent by the client in the request line."
+	case "http.request.resend_count":
+		return "The ordinal number of request resending attempt (for any reason, including redirects)."
+	case "http.response.header":
+		return "HTTP response headers, `<key>` being the normalized HTTP Header name (lowercase), the value being the header values."
+	case "http.response.status_code":
+		return "[HTTP response status code](https://tools.ietf.org/html/rfc7231#section-6)."
+	case "http.route":
+		return "The matched route, that is, the path template in the format used by the respective server framework."
 	default:
 		return ""
 	}
