d11f6df2922e4ce24ea5ea09288e2e25da05222f
Robb Kidd <robbkidd@honeycomb.io>
2024-06-24 14:22:15 -0700

    remove experimental http attr
    Response body size is still experimental. Don't include it.

--- otlp/semconv.go
+++ otlp/semconv.go
@@ -6,10 +6,6 @@ func GetDescriptionForOtelAttributeName(name string) string {
 		return "HTTP request method."
 	case "http.request.header":
 		return "HTTP request headers, `<key>` being the normalized HTTP Header name (lowercase), the value being the header values."
-	case "http.response.body.size":
-		return "The size of the response payload body in bytes. This is the number of bytes transferred excluding headers and" +
-			"is often, but not always, present as the [Content-Length](https://www.rfc-editor.org/rfc/rfc9110.html#field.content-length)" +
-			"header. For requests using transport encoding, this should be the compressed size."
 	default:
 		return ""
 	}
