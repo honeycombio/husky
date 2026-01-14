c4eadee004ccdbc05269d6a8f8cc7feabc3698eb
Maggy <maggyprajapati@honeycomb.io>
2026-01-14 15:46:40 -0500

    fix(husky): Updates span id links to use the BytesToSpanID function (#339)
    <!--
    Thank you for contributing to the project! 💜
    Please make sure to:
    - Chat with us first if this is a big change
      - Open a new issue (or comment on an existing one)
    - We want to make sure you don't spend time implementing something we
    might have to say No to
    - Add unit tests
    - Mention any relevant issues in the PR description (e.g. "Fixes #123")
    
    Please see our [OSS process
    document](https://github.com/honeycombio/home/blob/main/honeycomb-oss-lifecycle-and-practices.md#)
    to get an idea of how we operate.
    -->
    
    ## Which problem is this PR solving?
    
    - Howard reported an
    [issue](https://linear.app/honeycombio/issue/COR-1504/weird-span-id-gets-generated-when-submitting-trace-otel-json-with-span)
    stating that when a span id links are included the trace and ingested
    into Honeycomb they the resulting span link id are somehow changed.
    
    
    ## Short description of the changes
    
    
    - Here's how I found the change I ingest the sample file Howard
    provided. One of the function called during translation is
    `TranslateTraceRequest` which does a bunch of things to format the trace
    and within this function the thing that stood to me is that
    `trace.link.span_id` is generated using the function
    `hex.EncodeToString` and passing in `links.span_id`.
    
    - For `trace.link.span_id`, the code uses
    `hex.EncodeToString(slink.SpanId)` directly, but for all other span IDs
    (like `trace.span_id` and `trace.parent_id`), it uses the
    BytesToSpanID() function so I updated it to use this function and after
    re-ingesting the file I saw the correct span id listed under the
    `links.span_id`
    
    - unit tests are passing!

--- otlp/traces.go
+++ otlp/traces.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"compress/gzip"
 	"context"
-	"encoding/hex"
 	"io"
 	"sync"
 	"time"
@@ -270,7 +269,7 @@ func TranslateTraceRequest(ctx context.Context, request *collectorTrace.ExportTr
 						"trace.trace_id":       traceID,
 						"trace.parent_id":      spanID,
 						"trace.link.trace_id":  BytesToTraceID(slink.TraceId),
-						"trace.link.span_id":   hex.EncodeToString(slink.SpanId),
+						"trace.link.span_id":   BytesToSpanID(slink.SpanId),
 						"parent_name":          span.Name,
 						"meta.annotation_type": "link",
 						"meta.signal_type":     "trace",
